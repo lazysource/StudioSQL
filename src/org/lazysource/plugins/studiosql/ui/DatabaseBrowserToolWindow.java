08cd358f275ed786d2d943d82394dccbb7916ee0
Ishan Khanna <ishan1604@gmail.com>
2016-02-28 15:53:47 +0530

    Add: Ability to detect module names of the project

--- src/org/lazysource/plugins/studiosql/ui/DatabaseBrowserToolWindow.java
+++ src/org/lazysource/plugins/studiosql/ui/DatabaseBrowserToolWindow.java
@@ -1,17 +1,24 @@
 package org.lazysource.plugins.studiosql.ui;
 
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 import org.lazysource.plugins.studiosql.sqlite.SchemaReader;
-import org.lazysource.plugins.studiosql.utils.DBSucker;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -41,9 +48,30 @@ public class DatabaseBrowserToolWindow implements ToolWindowFactory,
     private JButton buttonRefresh;
 
     /**
+     * Module name selector. This allows you to choose a module within a
+     * project for which you want to select the package name.
+     */
+    private JComboBox moduleComboBox;
+
+    /**
+     * Package name of the application for which the database has to be pulled.
+     */
+    private JTextField packageName;
+
+    /**
+     * Name of the database that is being currently browsed.
+     */
+    private JTextField databaseName;
+
+    /**
      * The ToolWindow instance.
      */
     private ToolWindow mtoolWindow;
+
+    /**
+     * List of the Names of All the modules of the current project.
+     */
+    private List<String> moduleNameList;
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
@@ -53,13 +81,19 @@ public class DatabaseBrowserToolWindow implements ToolWindowFactory,
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(mainPanel, "", false);
 
+        tableTabbedPane.removeAll();
+
         registerButtonListeners();
+
+        this.moduleNameList = getAllModulesInTheProject(project);
+
+        updateModuleComboBox();
 
         toolWindow.getContentManager().addContent(content);
     }
 
     /**
-     * Registers the action listeners to all the components.
+     * Registers the action listeners for all the components.
      */
     private void registerButtonListeners() {
 
@@ -85,5 +119,39 @@ public class DatabaseBrowserToolWindow implements ToolWindowFactory,
 
         }
 
+    }
+
+    /**
+     *
+     * @param project
+     * @return
+     */
+    private List<String> getAllModulesInTheProject(Project project){
+
+        List<String> moduleNames = new ArrayList<>();
+
+        final ProjectFileIndex index = ProjectRootManager.getInstance(project).getFileIndex();
+
+        PsiFile[] filesByName = FilenameIndex.getFilesByName(project, "build.gradle", GlobalSearchScope.allScope(project));
+
+        for (PsiFile pf : filesByName) {
+
+            VirtualFile vf = pf.getVirtualFile();
+            Module module = index.getModuleForFile(vf);
+            if (module != null) {
+                moduleNames.add(module.getName());
+            }
+        }
+
+        return moduleNames;
+    }
+
+    private void updateModuleComboBox() {
+
+        for (String moduleName : moduleNameList) {
+            moduleComboBox.addItem(moduleName);
+        }
+
+        moduleComboBox.setEditable(false);
     }
 }
