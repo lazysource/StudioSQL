db84fa60231066eca0afa148f60e1e544bcf4202
Ishan Khanna <ishan1604@gmail.com>
2016-03-12 14:23:39 +0100

    Dirty branch

--- src/org/lazysource/plugins/studiosql/ui/DatabaseBrowserToolWindow.java
+++ src/org/lazysource/plugins/studiosql/ui/DatabaseBrowserToolWindow.java
@@ -22,8 +22,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
